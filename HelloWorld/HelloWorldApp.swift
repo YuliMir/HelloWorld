02a5f2f7472a0358e434c66df180a6b970020c0f
@YuliMir <juliya.mironovav@gmail.com>
2022-02-23 17:56:46 +0300

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Юлия Миронова on 23.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
